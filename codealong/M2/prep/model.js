628b33c46ccec6fb30628de2b7881f8ae4fe127f
Robert Lysebo <robertlysebo@gmail.com>
2022-11-15 17:01:18 +0100

    finished model attempt

--- codealong/M2/prep/model.js
+++ codealong/M2/prep/model.js
@@ -1,10 +1,97 @@
 const roomGame = {
-    roomA: {
-        doorToRooms: ['B', ]
+  roomA: {
+    doorToRoom: [
+      {
+        doorColor: "red",
+        doorName: "B",
+      },
+      {
+        doorColor: "green",
+        doorName: "D",
+      },
+    ],
+    roomKey: {
+      haveKey: true,
+      keyColor: "red",
     },
-    roomB: ,
-    roomC: ,
-    roomD: ,
-    roomE: ,
-    roomF: ,
-}
+  },
+  roomB: {
+    doorToRoom: [
+      {
+        doorColor: "red",
+        doorName: "A",
+      },
+      {
+        doorColor: "grey",
+        doorName: "C",
+      },
+      {
+        doorColor: "blue",
+        doorName: "E",
+      },
+    ],
+    roomKey: {
+      haveKey: true,
+      keyColor: "green",
+    },
+  },
+  roomC: {
+    doorToRoom: [
+      {
+        doorColor: "grey",
+        doorName: "B",
+      },
+    ],
+    roomKey: {
+      haveKey: true,
+      keyColor: "white",
+    },
+  },
+  roomD: {
+    doorToRoom: [
+      {
+        doorColor: "green",
+        doorName: "A",
+      },
+    ],
+    roomKey: {
+      haveKey: true,
+      keyColor: "blue",
+    },
+  },
+  roomE: {
+    doorToRoom: [
+      {
+        doorColor: "blue",
+        doorName: "B",
+      },
+      {
+        doorColor: "white",
+        doorName: "F",
+      },
+    ],
+    roomKey: {
+      haveKey: true,
+      keyColor: "grey",
+    },
+  },
+  roomF: {
+    doorToRoom: [
+      {
+        doorColor: "white",
+        doorName: "E",
+      },
+    ],
+    roomKey: {
+      haveKey: false,
+      keyColor: "",
+    },
+  },
+};
+
+/*
+Hvilke rom?
+Hvilke dører?
+Nøkkel i rommet?
+
+*/
