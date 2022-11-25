a808f32944386fcc33e581a61f5bb6335c173b63
Robert Lysebo <robertlysebo@gmail.com>
2022-11-25 15:53:48 +0100

    weekend additions

--- modul-2/div-tema/aimtraining.js
+++ modul-2/div-tema/aimtraining.js
@@ -12,6 +12,7 @@ const game = {
     reactionTime: [],
     startTime: 0,
     finishTime: 0,
+    misses: 0,
   },
 
   data: {
@@ -20,55 +21,55 @@ const game = {
         id: 123,
         name: "Robert",
         username: "Robhimself",
-        score: 200,
+        score: 0,
       },
       {
         id: 124,
         name: "Simen",
         username: "dankert",
-        score: 100,
+        score: 10,
       },
       {
         id: 125,
         name: "David",
         username: "David",
-        score: 350,
+        score: 50,
       },
     ],
     scores: [
       {
         id: 123,
-        time: 10.05,
+        time: 10.051,
         date: "2022-11-24T12:01:41.372Z",
       },
       {
         id: 123,
-        time: 10.15,
+        time: 10.151,
         date: "2022-11-23T12:02:41.372Z",
       },
       {
         id: 123,
-        time: 11.15,
+        time: 11.151,
         date: "2022-11-22T12:03:41.372Z",
       },
       {
         id: 123,
-        time: 7.15,
+        time: 7.151,
         date: "2022-11-24T12:04:41.372Z",
       },
       {
         id: 124,
-        time: 7.15,
+        time: 7.151,
         date: "2022-11-24T12:04:41.372Z",
       },
       {
         id: 125,
-        time: 8.15,
+        time: 8.151,
         date: "2022-11-24T12:04:41.372Z",
       },
       {
         id: 125,
-        time: 10.15,
+        time: 10.115,
         date: "2022-11-24T12:04:41.372Z",
       },
     ],
@@ -89,10 +90,10 @@ function updateView() {
     </div>
     <div class="info">
         <div class="info-text">
-            "For å starte spillet: Trykk på den gule sirkelen. Obs! Dette får timeren til å starte. Ett spill = 10 treff."
+          Ett spill = 10 treff.
         </div>
         <div class="results">
-            ${drawResult()}
+            ${game.input.reactionTime.length == 10 ? drawResult() : ""}
         </div>
         <div class="historic">
             ${userScores()}
@@ -123,30 +124,65 @@ function showLeaderboard() {
 function userScores() {
   const scores = game.data.scores;
   const list = [...scores].filter((score) => score.id === game.app.user);
-  console.log(list);
-  const userScore = list.map((user) => user.date).sort((a, b) => a.date - b.date);
-  console.log(userScore);
-}
+  list.sort((a, b) => {
+    if (a.date > b.date) {
+      return -1;
+    }
+    if (b.date > a.date) {
+      return 1;
+    }
+    return 0;
+  });
+  let userHtml = "";
+  for (let i = 0; i < list.length; i++) {
+    let date = new Date(list[i].date);
 
-// Controller
+    userHtml += /*HTML*/ `
+    <div class="userScore">
+      <div class="userScore-text">
+        ${list[i].time}s 
+      </div><div class="userScore-text">
+        ${date.toLocaleTimeString("no-NO", {
+          hour: "2-digit",
+          minute: "2-digit",
+        })},  ${date.toLocaleDateString("no-NO", { year: "2-digit", month: "2-digit", day: "2-digit" })}
+      </div>
+    </div>
+    `;
+  }
+  return userHtml;
+}
 
 function createBoard() {
   let gameHtml = "";
   let randomDiv = Math.floor(Math.random() * 25);
   for (let i = 0; i < 25; i++) {
-    if (randomDiv == i) {
-      gameHtml += `<div class="lightOn" onclick="hit()"></div>`;
-    } else {
-      gameHtml += `<div class="lamp"></div>`;
+    if (game.input.startTime == 0) {
+      if (i == 12) {
+        gameHtml += `<div class="lightOn" onclick="hit()">Start!</div>`;
+      } else {
+        gameHtml += `<div class="lamp" onclick="miss()"></div>`;
+      }
+    }
+    if (game.input.startTime != 0) {
+      if (randomDiv == i) {
+        gameHtml += `<div class="lightOn" onclick="hit()"></div>`;
+      } else {
+        gameHtml += `<div class="lamp" onclick="miss()"></div>`;
+      }
     }
   }
   return gameHtml;
 }
 
+// Controller
+
 function hit() {
   let reactionTime = game.input.reactionTime;
   if (game.input.startTime == 0) {
+    game.input.reactionTime = [];
     game.input.startTime = new Date().getTime();
+    game.input.misses = 0;
   } else {
     game.input.finishTime = new Date().getTime();
     let reaction = Math.floor(game.input.finishTime - game.input.startTime);
@@ -155,12 +191,16 @@ function hit() {
     game.input.finishTime = 0;
   }
   if (reactionTime.length >= 10) {
-    drawResult();
     game.input.startTime = 0;
-    game.input.finishTime = 0;
-  } else {
-    updateView();
   }
+  updateView();
+}
+
+function miss() {
+  if (game.input.startTime != 0) {
+    game.input.misses++;
+  }
+  console.log(game.input.misses);
 }
 
 function drawResult() {
@@ -169,26 +209,30 @@ function drawResult() {
   let userId = game.app.user;
   let showScore = "";
 
-  if (reactionTime.length == 0) {
-    return "";
-  }
-
-  let sum = reactionTime.reduce(function (a, b) {
+  const sum = reactionTime.reduce(function (a, b) {
     return a + b;
   }, 0);
 
-  let totalTime = sum / 1000;
+  const totalTime = sum / 1000;
+  const pts = 10 / totalTime;
 
   let newScore = {
     id: userId,
     time: totalTime,
+    points: pts,
     date: new Date().toISOString(),
   };
 
   scores.push(newScore);
   let bestTime = scores.filter((item) => item.id == userId);
   bestTime.sort((a, b) => {
-    return b.time - a.time;
+    if (a.time > b.time) {
+      return 1;
+    }
+    if (b.time > a.time) {
+      return -1;
+    }
+    return 0;
   });
   showScore += /*HTML*/ `
     <div>
@@ -196,7 +240,14 @@ function drawResult() {
             Din beste tid er: ${bestTime[0].time}
         </div>
     </div>`;
-  game.input.reactionTime = [];
-  console.log(bestTime);
   return showScore;
+}
+
+findScores();
+function findScores() {
+  const scores = game.data.scores;
+  let arr = [...scores];
+
+  let results = arr.group(({ id }) => id);
+  console.log(results);
 }
