075d9350d37af943c5d944b60c1cb5a2b83768f2
Robert Lysebo <robertlysebo@gmail.com>
2022-12-27 14:07:58 +0100

    Tutorial materials

---
+++ modul-3/PasswordGenerator/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace PasswordGenerator
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string lower = "abcdefghijklmnopqrstuvwxyz";
+            string upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string number = "0123456789";
+            string special = "!@#$%^*()[]{},.:~_-="; // avoiding problematic characters
+
+            string title = @"
+  ______                                              _   _____                                  _
+  | ___ \                                            | | |  __ \                                | |
+  | |_/ /  __ _  ___  ___ __      __  ___   _ __   __| | | |  \/  ___  _ __    ___  _ __   __ _ | |_   ___   _ __
+  |  __/  / _` |/ __|/ __|\ \ /\ / / / _ \ | '__| / _` | | | __  / _ \| '_ \  / _ \| '__| / _` || __| / _ \ | '__|
+  | |    | (_| |\__ \\__ \ \ V  V / | (_) || |   | (_| | | |_\ \|  __/| | | ||  __/| |   | (_| || |_ | (_) || |
+  \_|     \__,_||___/|___/  \_/\_/   \___/ |_|    \__,_|  \____/ \___||_| |_| \___||_|    \__,_| \__| \___/ |_|
+
+   __          ___       __   __   _               _____
+  / /  __ __  / _ \___  / /  / /  (_)_ _  ___ ___ / / _/
+ / _ \/ // / / , _/ _ \/ _ \/ _ \/ /  ' \(_-</ -_) / _/
+/_.__/\_, / /_/|_|\___/_.__/_//_/_/_/_/_/___/\__/_/_/
+     /___/
+                                                                                                            ";
+            Console.WriteLine(title);
+
+            if (args == null || args.Length == 0)
+            {
+                string noArgs = @"
+You did not provide any arguments...
+
+Options:
+  - l = lower case letter
+  - L = upper case letter
+  - d = digit
+  - s = special character (!""#¤%&/(){}[]
+
+Example: PasswordGenerator 14 lLssdd
+         Min. 1 lower case
+         Min. 1 upper case
+         Min. 2 special characters
+         Min. 2 digits";
+
+                Console.WriteLine(noArgs);
+            }
+            else
+            {
+            }
+        }
+    }
+}
---
+++ modul-3/QuickSolution/QuickTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("QuickTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("QuickTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("219d122f-7fce-4859-9645-9bc754d14a85")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
